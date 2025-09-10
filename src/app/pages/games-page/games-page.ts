36483bef642d6b3f4fc758f048741e0602c00ddf
Enrico Bregoli <bregolienrico@gmail.com>
2025-09-11 00:33:54 +0200

    Games page: aggiunto componente di paginazione (da sistemare non funziona)

--- src/app/pages/games-page/games-page.ts
+++ src/app/pages/games-page/games-page.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { GamesService } from './games-service';
 import { Game } from '../../models/game';
 import { Observable } from 'rxjs';
@@ -7,10 +7,11 @@ import { AsyncPipe } from '@angular/common';
 import { GameCard } from '../../shared/components/games/game-card/game-card';
 import { ApiResponse } from '../../models/api-response';
 import { CardSkeleton } from '../../shared/components/skeletons/card-skeleton/card-skeleton';
+import { Pagination } from "../../shared/components/pagination/pagination";
 
 @Component({
   selector: 'app-games-page',
-  imports: [AsyncPipe, GameCard, CardSkeleton],
+  imports: [AsyncPipe, GameCard, CardSkeleton, Pagination],
   templateUrl: './games-page.html',
   styleUrl: './games-page.css',
 })
@@ -19,6 +20,9 @@ export class GamesPage implements OnInit {
 
   numSkeleton = [...Array(20).keys()].map((i) => i + 1); // array di 20 interi
   games$!: Observable<ApiResponse<Game>>;
+
+  currentPage = signal(1);
+  pageSize = signal(20);
 
   ngOnInit(): void {
     this.games$ = this.gamesService.getAllGames();
