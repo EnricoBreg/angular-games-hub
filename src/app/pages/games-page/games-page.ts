ef17b8bed364d2d4c1c5d390b8bddb8711655dea
Enrico Bregoli <bregolienrico@gmail.com>
2025-09-10 21:49:05 +0200

    Games page: aggiunta visualizzazione degli skeleton e cambiato valore di ritorno utilizzato nell'observer dei giochi in modo da ritornare tutti i dati restituiti da backend. L'ultima modifica serve poi la pagination.

--- src/app/pages/games-page/games-page.ts
+++ src/app/pages/games-page/games-page.ts
@@ -1,28 +1,27 @@
-import { Component, inject, OnInit, signal, Signal } from '@angular/core';
-import { GamesService, Post } from './games-service';
+import { Component, inject, OnInit } from '@angular/core';
+import { GamesService } from './games-service';
 import { Game } from '../../models/game';
-import { Observable, of } from 'rxjs';
-import { catchError, map, startWith, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { AsyncPipe } from '@angular/common';
 import { GameCard } from '../../shared/components/games/game-card/game-card';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { ApiResponse } from '../../models/api-response';
+import { CardSkeleton } from '../../shared/components/skeletons/card-skeleton/card-skeleton';
 
 @Component({
   selector: 'app-games-page',
-  imports: [AsyncPipe, GameCard],
+  imports: [AsyncPipe, GameCard, CardSkeleton],
   templateUrl: './games-page.html',
   styleUrl: './games-page.css',
 })
 export class GamesPage implements OnInit {
   private gamesService = inject(GamesService);
-  games$!: Observable<Game[]>;
+
+  numSkeleton = [...Array(20).keys()].map((i) => i + 1); // array di 20 interi
+  games$!: Observable<ApiResponse<Game>>;
 
   ngOnInit(): void {
-    this.games$ = this.gamesService
-      .getAllGames()
-      .pipe(
-        map((response) => response.results),
-      );
+    this.games$ = this.gamesService.getAllGames();
   }
 }
 
